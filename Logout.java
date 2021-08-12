f7475499c925f80825398b275c1054a4423f2253
IMRAN AL MUNYEEM <45540113+imranalmunyeem@users.noreply.github.com>
2021-08-12 19:48:48 +0600

    Add files via upload

---
+++ Logout.java
@@ -0,0 +1,24 @@
+package ui;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+public class Logout {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\webdrivers\\chromedriver.exe");
+		//System.setProperty("webdriver.gecko.driver","C:\\webdrivers\\geckodriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		//FirefoxDriver driver = new FirefoxDriver();
+		driver.get("http://18.209.122.163/admin/login");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("email")).sendKeys("superadmin@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("superadmin");
+		driver.findElementByXPath("/html/body/div/div/div/div[1]/div/form/div[4]/div/button").click();
+		driver.findElementByXPath("/html/body/div/main/div/div/p[2]/a").click();
+		Thread.sleep(1000);
+		driver.close();
+		
+	}
+
+}

a278a601b868de39b94e1683c91a486f23187621
IMRAN AL MUNYEEM <45540113+imranalmunyeem@users.noreply.github.com>
2021-08-16 19:45:31 +0600

    Update AddSite.java

--- Mediusware Ltd/EasyAsk/Backend Testing/AddSite.java
+++ Mediusware Ltd/EasyAsk/Backend Testing/AddSite.java
@@ -16,17 +16,23 @@ public class AddSite {
 		driver.findElement(By.id("password")).sendKeys("superadmin");
 		driver.findElementByXPath("/html/body/div/div/div/div[1]/div/form/div[4]/div/button").click();
 		
+		//Search Site
+		driver.findElementByXPath("/html/body/div/div/nav/ul/li[11]/a").click();
+		driver.findElementByXPath("/html/body/div[2]/main/div[2]/div/div/div[1]/div[2]/div/div/label/input").sendKeys("Apple");
+		
+		//preview
+		driver.findElementByXPath("/html/body/div[2]/main/div[2]/div/div/div[2]/div[2]/div/table/tbody/tr/td[3]/a[1]").click();
+		driver.findElementByXPath("/html/body/div/main/div/div/div/div/div/table/tbody/tr[4]/td[2]/a[1]").click();
+		
+		
 		//Selecting Items in a Multiple SELECT elements
 		driver.findElementByXPath("/html/body/div/div/nav/ul/li[11]/a").click();
 		driver.findElementByXPath("/html/body/div[2]/main/div[2]/div/div/div[1]/div[1]/div/a/span").click();
 		driver.findElement(By.name("site_name")).sendKeys("Test By Imran");
 		driver.findElement(By.name("site_description")).sendKeys("Automation Test Script");
-		driver.findElement(By.name("url")).sendKeys("https://www.youtube.com/playlist?list=PLL34mf651faPB-LyEP0-a7Avp_RHO0Lsm");
+		driver.findElement(By.name("url")).sendKeys("https://www.1377x.to/sort-search/top%20gun/seeders/desc/1/");
 		driver.findElementByXPath("/html/body/div/main/div/div/div/form/div[2]/div/button/span[2]").click();
 		
-		
-		
-		Thread.sleep(3000);
 		driver.close();
 		
 	}

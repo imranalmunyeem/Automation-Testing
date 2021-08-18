21f1ed9ada7d0eb7bfae858c6ec9df9e803bfc15
IMRAN AL MUNYEEM <45540113+imranalmunyeem@users.noreply.github.com>
2021-08-18 19:32:29 +0600

    Add files via upload

---
+++ Mediusware Ltd/EasyAsk/Backend Testing/MarketingTest.java
@@ -0,0 +1,62 @@
+package ui;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+public class MarketingTest {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\webdrivers\\chromedriver.exe");
+		//System.setProperty("webdriver.gecko.driver","C:\\webdrivers\\geckodriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		//FirefoxDriver driver = new FirefoxDriver();
+		driver.get("http://18.209.122.163/admin/login");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("email")).sendKeys("superadmin@gmail.com");
+		Thread.sleep(1000);
+		driver.findElement(By.id("password")).sendKeys("superadmin");
+		Thread.sleep(1000);
+		driver.findElementByXPath("/html/body/div/div/div/div[1]/div/form/div[4]/div/button").click();
+		Thread.sleep(1000);
+		
+		//Marketing
+		driver.findElementByXPath("/html/body/div/div/nav/ul/li[16]/a").click();
+		Thread.sleep(1000);
+		
+		
+		//Campaigns 
+		driver.findElementByXPath("/html/body/div/div/nav/ul/li[16]/ul/li[1]/a").click();
+		Thread.sleep(2000);
+		
+		
+	    //LandingPages
+		driver.findElementByXPath("/html/body/div/div/nav/ul/li[16]/ul/li[2]/a").click();
+		Thread.sleep(2000);
+		
+		
+		//Merchandising Zone
+		driver.findElementByXPath("/html/body/div/div/nav/ul/li[16]/ul/li[3]/a").click();
+		Thread.sleep(2000);
+		
+		
+		//Email
+		driver.findElementByXPath("/html/body/div/div/nav/ul/li[16]/ul/li[4]/a").click();
+		Thread.sleep(2000);
+		
+		
+	    //Subscribers
+		driver.findElementByXPath("/html/body/div/div/nav/ul/li[16]/ul/li[5]/a").click();
+		Thread.sleep(2000);
+		
+		
+		//Log Out
+				driver.findElementByXPath("/html/body/header/ul[2]/li/a/img").click();
+				Thread.sleep(3000);
+				driver.findElementByXPath("/html/body/header/ul[2]/li/div/a[2]").click();
+				Thread.sleep(3000);
+				
+				driver.close();
+				
+				
+			}
+		}
---
+++ Mediusware Ltd/EasyAsk/Backend Testing/SettingsTest.java
@@ -0,0 +1,75 @@
+package ui;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+public class SettingsTest {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\webdrivers\\chromedriver.exe");
+		//System.setProperty("webdriver.gecko.driver","C:\\webdrivers\\geckodriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		//FirefoxDriver driver = new FirefoxDriver();
+		driver.get("http://18.209.122.163/admin/login");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("email")).sendKeys("superadmin@gmail.com");
+		Thread.sleep(1000);
+		driver.findElement(By.id("password")).sendKeys("superadmin");
+		Thread.sleep(1000);
+		driver.findElementByXPath("/html/body/div/div/div/div[1]/div/form/div[4]/div/button").click();
+		Thread.sleep(1000);
+		
+		//Settings
+		driver.findElementByXPath("/html/body/div/div/nav/ul/li[17]/a").click();
+		Thread.sleep(1000);
+		
+		
+		//Core Configuration
+		driver.findElementByXPath("/html/body/div/div/nav/ul/li[17]/ul/li[1]/a").click();
+		Thread.sleep(2000);
+		
+		
+		//Core Configuration Edit
+		driver.findElementByXPath("/html/body/div/main/div/div/div/div/div/table/tbody/tr[7]/td[2]/a").click();
+		Thread.sleep(2000);
+		driver.findElementByXPath("/html/body/div/main/div/div/div/form/div[2]/div/ul/li[2]/a").click();
+		Thread.sleep(2000);
+		driver.findElementByXPath("/html/body/div/main/div/div/div/form/div[3]/div/button/span[2]").click();
+		
+		
+		//ERP Configuration
+		driver.findElementByXPath("/html/body/div/div/nav/ul/li[17]/ul/li[2]/a").click();
+		Thread.sleep(2000);
+		
+		
+	    //Shipping
+		driver.findElementByXPath("/html/body/div/div/nav/ul/li[17]/ul/li[3]/a").click();
+		Thread.sleep(2000);
+		
+		
+		//TAX
+		driver.findElementByXPath("/html/body/div/div/nav/ul/li[17]/ul/li[4]/a").click();
+		Thread.sleep(2000);
+		
+		
+		//Payments
+		driver.findElementByXPath("/html/body/div/div/nav/ul/li[17]/ul/li[5]/a").click();
+		Thread.sleep(2000);
+		
+		
+	    //Email
+		driver.findElementByXPath("/html/body/div/div/nav/ul/li[17]/ul/li[6]/a").click();
+		Thread.sleep(2000);
+		
+		
+		//Log Out
+				driver.findElementByXPath("/html/body/header/ul[2]/li/a/img").click();
+				Thread.sleep(3000);
+				driver.findElementByXPath("/html/body/header/ul[2]/li/div/a[2]").click();
+				Thread.sleep(3000);
+				
+				driver.close();
+				
+				
+			}
+		}
